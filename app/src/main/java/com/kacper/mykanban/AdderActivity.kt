0d2f9bcf6e32146d3fc3d082dd543befe3252b30
kazaw <kacperzawadzki@gmail.com>
2020-09-11 15:13:57 +0200

    Add button usage and checking for filled fields

--- app/src/main/java/com/kacper/mykanban/AdderActivity.kt
+++ app/src/main/java/com/kacper/mykanban/AdderActivity.kt
@@ -1,9 +1,7 @@
 package com.kacper.mykanban
 
-import android.R.attr.country
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -11,16 +9,30 @@ class AdderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_adder)
+
         val spinner: Spinner = findViewById(R.id.spinner_colors)
         ArrayAdapter.createFromResource(
-            this,
-            R.array.array_colors,
-            android.R.layout.simple_spinner_item
+            this, R.array.array_colors, android.R.layout.simple_spinner_item
         ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
             spinner.adapter = adapter
+        }
+        val buttonInsert: Button = findViewById(R.id.button_insert)
+        val editTextName : EditText = findViewById(R.id.editText_name)
+        val editTextDescription : EditText = findViewById(R.id.editText_description)
+        buttonInsert.setOnClickListener {
+            if(editTextName.text.isEmpty() || editTextDescription.text.isEmpty() ){
+                val toast = Toast.makeText(this, "Fill all fields", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+            else{
+                try {
+                    finish()
+                }
+                catch (e : Exception){
+                    e.printStackTrace()
+                }
+            }
         }
     }
 }
