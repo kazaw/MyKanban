7f35bb153a7c7b8fba98a19d90e1854dfd284b53
kazaw <kacperzawadzki@gmail.com>
2020-09-12 22:01:51 +0200

    Add implementation for OnActivityResult and ViewModel creation

--- app/src/main/java/com/kacper/mykanban/MainActivity.kt
+++ app/src/main/java/com/kacper/mykanban/MainActivity.kt
@@ -3,11 +3,16 @@ package com.kacper.mykanban
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.tabs.TabLayout
+import com.kacper.mykanban.data.KanbanCard
 import com.kacper.mykanban.ui.main.SectionsPagerAdapter
 import com.kacper.mykanban.utilities.REQUEST_INSERT
+import com.kacper.mykanban.viewmodel.KanbanCardViewModel
+import com.kacper.mykanban.viewmodel.KanbanCardViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,11 +24,26 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = findViewById(R.id.fab)
 
+        var kanbanCardViewModelFactory = KanbanCardViewModelFactory(application)
+        var kanbanCardViewModel = ViewModelProvider(this, kanbanCardViewModelFactory).get(KanbanCardViewModel::class.java)
+/*        kanbanCardViewModel.all.observe(this, Observer { cards ->
+            cards.let {}//TODO: SOMETHING
+        })*/
+
+
+        val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener {
             val intent = Intent(this@MainActivity, AdderActivity::class.java)
             startActivityForResult(intent, REQUEST_INSERT)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_INSERT && resultCode == RESULT_OK) {
+            val kanbanCard : KanbanCard = data?.getSerializableExtra("KanbanCardExtra") as KanbanCard
+
         }
     }
 }
