097524c44828eda8574315df5ba8ab6144403ffb
kazaw <kacperzawadzki@gmail.com>
2020-09-14 15:02:15 +0200

    Add menu actions and working card details dispaly

--- app/src/main/java/com/kacper/mykanban/ui/main/KanbanCardDetailFragment.kt
+++ app/src/main/java/com/kacper/mykanban/ui/main/KanbanCardDetailFragment.kt
@@ -1,20 +1,22 @@
 package com.kacper.mykanban.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.kacper.mykanban.R
 import com.kacper.mykanban.data.KanbanCard
 import com.kacper.mykanban.viewmodel.KanbanCardDetailViewModel
 import com.kacper.mykanban.viewmodel.KanbanCardDetailViewModelFactory
-import com.kacper.mykanban.viewmodel.KanbanCardViewModelFactory
-import kotlinx.android.synthetic.main.kanban_card_detail_fragment.*
+import kotlinx.android.synthetic.main.fragment_kanbancard_detail.*
+import java.text.SimpleDateFormat
 
-class KanbanCardDetailFragment : DialogFragment() {
+class KanbanCardDetailFragment : DialogFragment(), Toolbar.OnMenuItemClickListener   {
 
     companion object {
 
@@ -25,29 +27,74 @@ class KanbanCardDetailFragment : DialogFragment() {
             val args = Bundle().apply {
                 putSerializable(KANBAN_KEY, kanbanCard)
             }
+            fragment.arguments = args
             return fragment
         }
     }
 
     private lateinit var viewModelFactory: KanbanCardDetailViewModelFactory
     private lateinit var viewModel: KanbanCardDetailViewModel
+    private lateinit var kanbanCard: KanbanCard
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModelFactory = KanbanCardDetailViewModelFactory(activity!!.application)
+        viewModel = ViewModelProviders.of(this, viewModelFactory).get(
+            KanbanCardDetailViewModel::class.java
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.kanban_card_detail_fragment, container, false)
+        return inflater.inflate(R.layout.fragment_kanbancard_detail, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        toolbar.setNavigationOnClickListener{
+        toolbar_detail.setNavigationOnClickListener{
             dismiss()
         }
-        viewModelFactory = KanbanCardDetailViewModelFactory(activity!!.application)
-        viewModel = ViewModelProviders.of(this, viewModelFactory).get(
-            KanbanCardDetailViewModel::class.java)
-        // TODO: Use the ViewModel
+        toolbar_detail.inflateMenu(R.menu.menu_detail)
+        toolbar_detail.setOnMenuItemClickListener(this)
+        arguments?.getSerializable(KANBAN_KEY)?.let { kanbanCard = it as KanbanCard }
+        viewModel.getKanbanCard(kanbanCard).observe(viewLifecycleOwner, Observer {
+            this.kanbanCard = it
+            displayDetails()
+        })
     }
+    private fun displayDetails(){
+        val simpleDateFormat = SimpleDateFormat("EEEE, MMMM d, yyyy 'at' h:mm a")
+        textView_detail_name.text = kanbanCard.name
+        textView_detail_type.text = kanbanCard.type
+        textView_detail_date.text = simpleDateFormat.format(kanbanCard.calendar.time)
+        textView_detail_description.text = kanbanCard.description
+        toolbar_detail.setBackgroundColor(kanbanCard.color)
+
+    }
+    private fun deleteCurrent(){
+        viewModel.delete(kanbanCard)
+        dismiss()
+    }
+
+    private fun startEditActivity(){
+
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        return when (item?.itemId){
+            R.id.action_edit -> {
+                startEditActivity()
+                true
+            }
+            R.id.action_delete -> {
+                deleteCurrent()
+                true
+            }
+            else -> false
+        }
+    }
+
 
 }
