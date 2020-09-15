d93d8e5cceb975f5f338357b0cc5f6219a6085e1
kazaw <kacperzawadzki@gmail.com>
2020-09-15 11:35:16 +0200

    getItem return fragment with different types

--- app/src/main/java/com/kacper/mykanban/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/kacper/mykanban/ui/main/SectionsPagerAdapter.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.kacper.mykanban.R
+import com.kacper.mykanban.utilities.DONE
+import com.kacper.mykanban.utilities.IN_PROGRESS
+import com.kacper.mykanban.utilities.TO_DO
+import java.lang.Exception
 
 private val TAB_TITLES = arrayOf(
         R.string.tab_text_1,
@@ -20,7 +24,14 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
     : FragmentPagerAdapter(fm) {
 
     override fun getItem(position: Int): Fragment {
-        return KanbanCardFragment() //TODO: Switch, return fragment with kanbanCard with suitable type
+        //return KanbanCardFragment() //TODO: Switch, return fragment with kanbanCard with suitable type
+        return when(position){
+            0 -> KanbanCardFragment.newInstance(1, TO_DO)
+            1 -> KanbanCardFragment.newInstance(1, IN_PROGRESS)
+            2 -> KanbanCardFragment.newInstance(1, DONE)
+            else -> throw Exception("getItem Exception")
+        }
+
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
